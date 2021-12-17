113efd9cef7ab51bcd32472db211f63893200cb8
edgarperez97 <edgarperez_97@hotmail.com>
2021-12-17 11:45:30 -0600

    Version final de main y se adicion de funciones en common

--- criptoJoker/common.py
+++ criptoJoker/common.py
@@ -1,4 +1,8 @@
 import random
+import signal
+import sys
+import time
+import os
 
 from cryptography.fernet import Fernet
 from Crypto.PublicKey import RSA
@@ -19,11 +23,27 @@ __birthday_messages = (
     'ELjajas',
     ' :p '
 )
+#MANEJO DEL Ctrl. + C
+def def_handler(sig, frame):
+    print("\nSaliendo...\n")
+    sys.exit()
 
+signal.signal(signal.SIGINT, def_handler)
+
+#LIMPIAR PANTALLA
+def limpiar_pantalla():
+    comando = 'clear'
+    if os.name in ('nt', 'dos'):  # Si lla maquina esta corriendo Windows se utiliza cls
+        comando = 'cls'
+    os.system(comando)
+
+
+#SELECCIONAR UN MENSAJE ALEATORIO
 def elegir_mensaje():
     b_message = random.choice(__birthday_messages)
     b_message.encode()
     return b_message
+
 
 #LLAVES RSA
 def generar_llaves_RSA():
@@ -52,6 +72,73 @@ def generar_clave_AES():
 def obtener_llaves_AES():
     return open('./criptoJoker/keys/llave_AES.txt', 'rb').read()
 
-def preguntas():
-    pass
 
+##MENUS
+def pedir_joker():
+    print(
+        """
+        ________________________________________________________
+            Bienvenido a la fiesta del Joker, ¿Que deseas?      
+        ________________________________________________________
+        
+        Opciones disponibles:
+            1. Descifrar la Tarjeta
+            0. Salir
+        """
+    )
+
+def menu():
+    while True:
+        pedir_joker()
+        try:
+            entrada_usuario = int(input("Seleccione una opcion: "))
+
+            if entrada_usuario in range(2):
+
+                print(f"Usted eligió la opcion {entrada_usuario} !\n")
+                # print()
+
+                if entrada_usuario == 0:
+                    print("Adios! Vuelva pronto")
+                    quit()
+                elif entrada_usuario == 1:
+
+                    print("Antes resuelva la siguiente pregunta: ")
+
+                    banco_preguntas = {
+                        "3 + 3":6,
+                        "1 + 1":2,
+                        "2 + 2":4,
+                        "8 + 1":9,
+                        "7 - 3":4
+                    }
+
+                    pregunta, respuesta = random.choice(list(banco_preguntas.items()))
+
+                    print(pregunta)
+                    respuesta_a_pregunta = int(input())
+
+                    if respuesta == respuesta_a_pregunta:
+                        print(f"La espuesta es: {respuesta_a_pregunta}, por lo tanto su respuesta es CORRECTA\n")
+                        limpiar_pantalla()
+                        break
+                    else:
+                        print("Te equivocaste, Adios perdedor!!!")
+                        quit()
+
+
+            else:
+                print('Error, solo de aceptan numeros del 0 al 4')
+                time.sleep(1.2)
+                limpiar_pantalla()
+                # quit()
+
+        except ValueError:
+            print("Error, ingrese solamente numeros")
+            # quit()
+
+def guardar(nombre_archivo, archivo):
+    ruta = f'./criptoJoker/files/{nombre_archivo}.txt'
+    with open(ruta, mode='w')as file:
+        archivo = str(archivo)
+        file.write(archivo)
--- main.py
+++ main.py
@@ -3,27 +3,44 @@ from criptoJoker import sobre
 from criptoJoker import common as cm
 
 if __name__ == '__main__':
-    ####ESTO ES PARA VER EL FUNCIONAMIENTO DE TARJETA
+
     cm.generar_clave_AES()
+    print("Se generaron la nueva llave AES")
+
     tarjeta_ = tarjeta.Tarjeta(
         mensaje=cm.elegir_mensaje().encode(), 
         clave_aes=cm.obtener_llaves_AES()
     )
-    print(f'clave AES:\t\t{tarjeta_.clave_aes.decode()} \n')
-    print(f'tarjeta cifrada:\t{tarjeta_.mostrar_tarjeta_cifrada()} \n')
-    print(f'tarjeta descifrada:\t{tarjeta_.mostrar_tarjeta_descifrada()} \n')
-    print(f'mensaje:\t\t{tarjeta_.mensaje.decode()}\n\n\n')
 
-
-    print('*'*150)
-    print('\n')
-
-    ####ESTO ES PARA VER EL FUNCIONAMIENTO DE SOBRE
     sobre_ = sobre.Sobre(
         clave_aes=cm.obtener_llaves_AES(), 
         llave_pub_rsa=cm.obtener_llave_pub_RSA(), 
         llave_priv_rsa=cm.obtener_llave_priv_RSA()
     )
-    print(f'clave AES:\t\t{sobre_.clave_aes.decode()}\n')
-    print(f'sobre cifrado:\n{sobre_.mostrar_sobre_cifrado()} \n')
-    print(f'sobre descifrado:\t{sobre_.mostrar_sobre_descifrado().decode()} \n')
+
+    print(f'Esta es su Tarjeta: \t{tarjeta_.mostrar_tarjeta_cifrada()}\n')
+    cm.guardar(
+        'tarjeta', 
+        tarjeta_.mostrar_tarjeta_cifrada()
+    )
+    print(f'Este es su Sobre: \t{sobre_.mostrar_sobre_cifrado()}\n')
+    cm.guardar(
+        'Sobre',
+        sobre_.mostrar_sobre_cifrado()
+    )
+
+
+    cm.menu()
+    cm.limpiar_pantalla()
+
+
+    print(f'Este es su Sobre Descifrado: {sobre_.mostrar_sobre_descifrado().decode()} \n')
+    cm.guardar(
+        'sobre-descifrado',
+        sobre_.mostrar_sobre_descifrado().decode()
+    )
+    print(f'Esta es su Tarjeta Descifrada, asi que su mensaje original era "{tarjeta_.mostrar_tarjeta_descifrada()}" \n')
+    cm.guardar(
+        'tarjeta-descifrada-\'MENSAJE\'',
+        tarjeta_.mostrar_tarjeta_descifrada()
+    )
